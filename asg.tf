3297848d3d23508aa05778e678cde5495cb05837
Pawel Fiuto <pawel@bashton.com>
2017-06-23 11:13:48 +0200

    Add asg notification and sns topic

---
+++ asg.tf
@@ -0,0 +1,13 @@
+# ASG notification
+resource "aws_autoscaling_notification" "asg_notification" {
+  group_names = [
+    "${var.asg_name}",
+  ]
+
+  notifications = [
+    "autoscaling:TEST_NOTIFICATION",
+    "autoscaling:EC2_INSTANCE_LAUNCH",
+  ]
+
+  topic_arn = "${aws_sns_topic.manage_dns_asg_sns.arn}"
+}
