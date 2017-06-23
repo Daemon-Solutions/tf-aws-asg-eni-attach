f8ec8c5abf5b652808b07fe3a29730256f1a8133
Pawel Fiuto <pawel@bashton.com>
2017-06-23 11:49:39 +0200

    Add missing lambda permission for sns topic

--- iam.tf
+++ iam.tf
@@ -67,3 +67,11 @@ resource "aws_iam_role_policy" "lambda_eni_attach_policy" {
 }
 EOF
 }
+
+resource "aws_lambda_permission" "asg_sns" {
+  statement_id  = "AllowExecutionFromSNS"
+  action        = "lambda:InvokeFunction"
+  function_name = "${aws_lambda_function.eni_attach.arn}"
+  principal     = "sns.amazonaws.com"
+  source_arn    = "${aws_sns_topic.asg_sns.arn}"
+}
