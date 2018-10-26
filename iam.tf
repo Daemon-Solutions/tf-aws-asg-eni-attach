275231f9daf4e05bae21d65b51270928de76186e
Pawel Fiuto <pawel.fiuto@claranet.uk>
2018-10-26 17:02:00 +0100

    avoid duplicate names

--- iam.tf
+++ iam.tf
@@ -1,6 +1,6 @@
 # Lambda role
 resource "aws_iam_role" "lambda_role" {
-  name = "${var.envname}-${var.service}-eni-attach-lambda-role"
+  description = "${var.envname}-${var.service}lambda"
 
   assume_role_policy = <<EOF
 {
@@ -21,7 +21,6 @@ EOF
 
 # Lambda policy for managing logs
 resource "aws_iam_role_policy" "lambda_logging_policy" {
-  name = "${var.envname}-${var.service}-lambda-eni-attach-logging"
   role = "${aws_iam_role.lambda_role.id}"
 
   policy = <<EOF
@@ -44,7 +43,6 @@ EOF
 
 # Lambda policy for attaching ENI
 resource "aws_iam_role_policy" "lambda_eni_attach_policy" {
-  name = "${var.envname}-${var.service}-lambda-eni-attach"
   role = "${aws_iam_role.lambda_role.id}"
 
   policy = <<EOF
