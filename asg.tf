738826c764b8200254f1657f2caa9a1f4ea5da75
Pawel Fiuto <pawel.fiuto@claranet.uk>
2018-10-26 17:43:24 +0100

    Paramtrise default result for lifecycle hook

--- asg.tf
+++ asg.tf
@@ -2,7 +2,7 @@
 resource "aws_autoscaling_lifecycle_hook" "asg_hook" {
   name                   = "${var.envname}-${var.service}-eni-attach-hook"
   autoscaling_group_name = "${var.asg_name}"
-  default_result         = "CONTINUE"
-  heartbeat_timeout      = 60
+  default_result         = "${var.lifecycle_hook_default_result}"
+  heartbeat_timeout      = 300
   lifecycle_transition   = "autoscaling:EC2_INSTANCE_LAUNCHING"
 }
